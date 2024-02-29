3b57469b1c5c67e76dedc674686b9c79b7bbb043
Emma Fox <emmajfox90@gmail.com>
2024-02-29 12:52:00 +0000

    Add new function get_data to refactor get_holidays_taken and get_over_hours

--- run.py
+++ run.py
@@ -12,52 +12,35 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSREAD_CLIENT.open("honest_hours")
 
-holidays_taken = SHEET.worksheet('holidays_taken')
-
-data = holidays_taken.get_all_values()
-
-def get_holidays_taken():
+def get_data(data_type):
     """
-    Get data for holidays taken that month
+    Get data for variable , validate data and return in string form
     """
-    print("Please enter holidays taken for the relevant employee below")
-    print("This should be in numerical form")
-    holidays_emma = input("Emma: ")
-    holidays_charlie = input("Charlie: ")
-    holidays_darren = input("Darren: ")
-    holidays_george = input("George: ")
-    holidays_conor = input("Conor: ")
-    
-    print(f"Emma took {holidays_emma}. Charlie took {holidays_charlie}. Darren took {holidays_darren}. George took {holidays_george}. Conor took {holidays_conor}.")
-    holidays_str = [str(holidays_emma), str(holidays_charlie), str(holidays_darren), str(holidays_george), str(holidays_conor)]
-    validate_data(holidays_str)
-    return holidays_str
-
-def get_over_hours():
-    """
-    Get data for over hours worked this month
-    """
-    print("Please enter the over hours worked by the relevant employee below")
-    print("This should be in numerical form")
-    hours_emma = input("Emma: ")
-    hours_charlie = input("Charlie: ")
-    hours_darren = input("Darren: ")
-    hours_george = input("George: ")
-    hours_conor = input("Conor: ")
-    
-    print(f"Emma worked {hours_emma}.\n Charlie worked {hours_charlie}.\n Darren worked {hours_darren}.\n George worked {hours_george}.\n Conor worked {hours_conor}.")
-    hours_str = [str(hours_emma), str(hours_charlie), str(hours_darren), str(hours_george), str(hours_conor)]
-    validate_data(hours_str)
-    return hours_str
+    print(f"Please enter {data_type} for the relevant employee below.")
+    print("This should be in numerical form.")
+    emma = input("Emma: ")
+    charlie = input("Charlie: ")
+    darren = input("Darren: ")
+    george = input("George: ")
+    conor = input("Conor: ")
+    data_str = [str(holidays_emma), str(holidays_charlie), str(holidays_darren), str(holidays_george), str(holidays_conor)]
+    validate_data(data_str)
+    return data_str
 
 
 def validate_data(values):
+    """
+    This function validates the data 
+    """
     try:
         [int(value) for value in values]
     except ValueError as e:
         print(f'Invalid answer: {e}. Please try again\n')
 
-
-get_holidays_taken()
-get_over_hours()
+def main():
+    """
+    Calls the main functions
+    """
+    get_data("holidays taken")
+    get_data("over hours worked")
 
