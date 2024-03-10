433080a5bd201907ac22ca070056246d42b0e96c
Emma Fox <emmajfox90@gmail.com>
2024-03-10 11:29:13 +0000

    Add line spacing for deployment

--- run.py
+++ run.py
@@ -65,7 +65,7 @@ def validate_word_in_list(word, given_list, list_name):
     try:
         check = word + ""
         if not word in given_list:
-            print(f"\n{word} is not in {list_name}. Please check the spelling and try again.")
+            print(f"{word} is not in {list_name}. Please check the spelling and try again.\n")
             return False
     except ValueError as e:
         print(f"Invalid data: {e}")
@@ -111,7 +111,7 @@ def calculate_total_holidays(holidays_taken, hours, employee):
     Gets the over time hours in terms of days and adds to holidays left
     returns holidays left
     """
-    print("Calculating holidays left...")
+    print("Calculating holidays left...\n")
 
     total_hrs_in_days = hours / 8
     worksheet = SHEET.worksheet(employee)
@@ -133,7 +133,7 @@ def calculate_pay_for_overtime(data, month):
     Takes over time hours and converts to wages
     """
     pay_out_for_month = data * 11
-    print(f"Overtime pay for {month} is €{pay_out_for_month}")
+    print(f"Overtime pay for {month} is €{pay_out_for_month}\n")
     
     return pay_out_for_month
 
@@ -146,7 +146,7 @@ def calculate_all_overtime_owed(employee):
     overtime_values = overtime_values[1:]
     overtime_integers = [int(overtime_value) for overtime_value in overtime_values]
     full_overtime_pay = sum(overtime_integers) 
-    print(f"You're overtime pay from January comes to €{full_overtime_pay}")
+    print(f"You're overtime pay from January comes to €{full_overtime_pay}\n")
 
     return full_overtime_pay
 
@@ -155,10 +155,10 @@ def update_sheet(employee, data, month):
     """
     Update employees worksheet with the month, holidays and overhours entered.
     """
-    print(f"Updating {employee}'s worksheet...")
+    print(f"Updating {employee}'s worksheet...\n")
     worksheet = SHEET.worksheet(employee)
     worksheet.append_row(data)
-    print(f"Worksheet updated for {month}.")
+    print(f"Worksheet updated for {month}.\n")
 
 def cash_out():
     """
@@ -166,7 +166,7 @@ def cash_out():
     """
     while True:
         print("Would you like to cash out your overtime?")
-        answer = input("Y/N: ")
+        answer = input("Y/N: \n")
         lower_answer = answer.lower()
         if validate_word_in_list( lower_answer, yes_no, "Y/N"):
             break
@@ -179,13 +179,13 @@ def cash_out_full_or_month(answer):
     if answer == "y" or answer == "yes":
         while True:
             print("Would you like to cash out your overtime in full from January or just for the last month?")
-            answer_two = input("Full/ Month: ")
+            answer_two = input("Full/ Month: \n")
             if validate_word_in_list(answer_two, full_month, "Full/Month"):
                 break
         return answer_two
 
     else:
-        print("Thank you for filling out your hours with honesty!")
+        print("\nThank you for filling out your hours with honesty!")
         quit()
 
 def cash_out_payment(answer, pay_out, month_pay_out, employee, hours):
@@ -195,15 +195,15 @@ def cash_out_payment(answer, pay_out, month_pay_out, employee, hours):
         pay_out_str = ["Paid out", "",  - all_hours, -days, - pay_out]
         worksheet = SHEET.worksheet(employee)
         worksheet.append_row(pay_out_str)
-        print(f"You will be receive €{pay_out} gross in your next paycheck")
-        print(f"Thank you for using honest hours")
+        print(f"You will be receive €{pay_out} gross in your next paycheck\n")
+        print(f"\nThank you for using honest hours")
         quit()
 
     else:
         days = hours/ 8
         pay_out_str_month = ["Paid out", "", - hours, - days, - month_pay_out]
-        print(f"You will be receive €{month_pay_out} gross in your next paycheck")
-        print(f"Thank you for using honest hours")
+        print(f"You will be receive €{month_pay_out} gross in your next paycheck\n")
+        print(f"\nThank you for using honest hours")
         quit()
 
    
